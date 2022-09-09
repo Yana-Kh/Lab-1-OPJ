30e0f3a0c6b9167d194f60bf66f38ba4f721e718
Yana-Kh <ynakh2017@mail.ru>
2022-09-09 23:18:51 +0300

    3

---
+++ OPJ_lab1.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0,"");
+
+    int staj, age;
+    string job, name;
+
+    cout << "Введите стаж (лет), место работ, имя и возраст (лет)";
+    cin >> staj >> job >> >> name >> age;
+    
+    cout << "Изменения сохранены";
+    system("pause");
+}
+
+
